a5399526ace40a4330396c003e2283471ce2e565
Nicolas Filippelli <nicolasfilippelli22@gmail.com>
2025-09-20 19:16:36 -0300

    Adds aimedSpawnItem and refactored freefallSpawnItem

--- scripts/SpawnHandler.cs
+++ scripts/SpawnHandler.cs
@@ -1,25 +1,51 @@
-using System;
 using Godot;
 
 public partial class SpawnHandler : Node2D
 {
     [Export]
+    public float forceMultiplier = 1.0F;
+
+    [Export]
     public PackedScene nodeToSpawnPath;
+    private bool _wasMousePressed = false;
 
     public override void _Process(double delta)
     {
+        bool isMousePressed = Input.IsMouseButtonPressed(MouseButton.Left);
+
         if (Input.IsActionJustPressed("spawn"))
         {
-            SpawnItem();
+            FreefallSpawnItem();
         }
+
+        if (isMousePressed && !_wasMousePressed)
+        {
+            AimedSpawnItem(GetGlobalMousePosition());
+        }
+        _wasMousePressed = isMousePressed;
     }
 
-    public void SpawnItem(Vector2? location = null)
+    public void FreefallSpawnItem(Vector2? spawnPosition = null)
     {
-        Vector2 spawnLocation = location ?? new Vector2(GlobalPosition.X, GlobalPosition.Y);
+        Vector2 _spawnPosition = spawnPosition ?? GlobalPosition;
+
+        AimedSpawnItem(_spawnPosition);
+    }
+
+    public void AimedSpawnItem(Vector2 aimedPosition, Vector2? spawnPosition = null)
+    {
+        Vector2 _spawnPosition = spawnPosition ?? GlobalPosition;
         Node2D node = nodeToSpawnPath.Instantiate<Node2D>();
         AddChild(node);
-        node.GlobalPosition = spawnLocation;
-        GD.Print("SPAWN!");
+        node.GlobalPosition = new Vector2(
+            _spawnPosition.X + (float)GD.RandRange(-0.1, 0.1),
+            _spawnPosition.Y
+        );
+
+        if (node is RigidBody2D rigidBody)
+        {
+            Vector2 forceDirection = (aimedPosition - _spawnPosition) * forceMultiplier;
+            rigidBody.ApplyForce(forceDirection);
+        }
     }
 }
