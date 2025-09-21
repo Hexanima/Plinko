8b01139965461ca27234b9e0478cb9892ea9eae8
Nicolas Filippelli <nicolasfilippelli22@gmail.com>
2025-09-20 22:53:32 -0300

    Adds random color to ball

---
+++ scripts/Ball.cs
@@ -0,0 +1,16 @@
+using Godot;
+
+public partial class Ball : RigidBody2D
+{
+    private MeshInstance2D mesh;
+    private PointLight2D light;
+
+    public override void _Ready()
+    {
+        mesh = GetNode<MeshInstance2D>("RoundShape/MeshInstance2D");
+
+        Color randomColor = new Color(GD.Randf(), GD.Randf(), GD.Randf());
+        Modulate = randomColor;
+        light.Color = randomColor;
+    }
+}
