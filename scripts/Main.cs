5210e787143fd4e3b8f39c61c45e50f1b7645140
Nicolas Filippelli <nicolasfilippelli22@gmail.com>
2025-09-20 22:54:23 -0300

    Adds new afk system

--- scripts/Main.cs
+++ scripts/Main.cs
@@ -2,5 +2,34 @@ using Godot;
 
 public partial class Main : Node2D
 {
-    public override void _Ready() { }
+    private bool afk = false;
+    private Node2D label;
+    private SpawnHandler spawnHandler;
+
+    public override void _Ready()
+    {
+        label = GetNode<Node2D>("AfkText");
+        spawnHandler = GetNode<SpawnHandler>("SpawnHandler");
+    }
+
+    public override void _Process(double delta)
+    {
+        if (Input.IsActionJustPressed("afk"))
+            afk = !afk;
+
+        label.Visible = afk;
+    }
+
+    public void _on_timer_timeout()
+    {
+        if (afk)
+        {
+            spawnHandler.AimedSpawnItem(
+                new Vector2(
+                    spawnHandler.GlobalPosition.X + GD.RandRange(-500, 500),
+                    spawnHandler.GlobalPosition.Y + GD.RandRange(-500, 500)
+                )
+            );
+        }
+    }
 }
